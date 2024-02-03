feac95befd13d62771ec4210da3237d6ecd81170
Victor <44504047+redheadgektor@users.noreply.github.com>
2024-02-03 18:44:36 +0400

    added new params and features
    Now you can start building all or individual bundles

--- Assets/Content/Content+Build.cs
+++ Assets/Content/Content+Build.cs
@@ -7,6 +7,9 @@ using UnityEditor;
 using System.Collections.Generic;
 using UnityEditor.Build.Pipeline;
 using BuildCompression = UnityEngine.BuildCompression;
+using UnityEditor.Build.Content;
+using CompressionType = UnityEngine.CompressionType;
+using System;
 
 public partial class Content : ScriptableObject, IPostprocessBuildWithReport
 {
@@ -46,7 +49,183 @@ public partial class Content : ScriptableObject, IPostprocessBuildWithReport
 
     void IPostprocessBuildWithReport.OnPostprocessBuild(BuildReport report) { }
 
-    void Build()
+    public ContentBuildFlags BuildFlags = ContentBuildFlags.None;
+
+    [Header(
+        "Packs assets in bundles contiguously based on the ordering of the source asset which results in improved asset loading times."
+    )]
+    public bool ContiguousBundles = true;
+
+    [Header(
+        "Assume sub Assets have no visible asset representations (are not visible in the Project view) which results in improved build times. Sub Assets in the built bundles cannot be accessed by AssetBundle.LoadAsset<T> or AssetBundle.LoadAllAssets<T>."
+    )]
+    public bool DisableVisibleSubAssetRepresentations = false;
+
+    [Header("Cache")]
+    public bool UseCache = true;
+
+    [Header("Write link.xml")]
+    public bool WriteLinkXML = false;
+
+    public void BuildSingle(Addon addon, Bundle bundle)
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().isDirty)
+        {
+            EditorUtility.DisplayDialog(
+                "ContentDatabase - Building",
+                "Unable to start archiving! There are unsaved scenes!",
+                "Close"
+            );
+            return;
+        }
+
+        AssetDatabase.SaveAssets();
+        //ignore if no assets
+        if (bundle.AssetsCount() == 0)
+        {
+            Debug.LogWarning($"[{nameof(Content)}] Bundle {bundle.name} ignored! (No Assets!)");
+            return;
+        }
+
+        var build_params = new ContentBuildParameters(
+            EditorUserBuildSettings.activeBuildTarget,
+            BuildTargetGroup.Standalone,
+            ContentFolder
+        )
+        {
+            WriteLinkXML = WriteLinkXML,
+            TempOutputFolder = $"{ContentFolder}/.TempContent",
+            UseCache = UseCache,
+            ContentBuildFlags = BuildFlags,
+            ContiguousBundles = ContiguousBundles,
+            DisableVisibleSubAssetRepresentations = DisableVisibleSubAssetRepresentations,
+            BundleCompression = BuildCompression.Uncompressed
+        };
+
+        var bundles = new List<AssetBundleBuild>();
+
+        var addon_bundle = $"{addon.Name}/{bundle.name}";
+        build_params.PerBundleCompression.Add(
+            addon_bundle,
+            GetCompressionType(addon.Name, bundle.name)
+        );
+        var abb = new AssetBundleBuild();
+        abb.assetBundleName = addon_bundle;
+        abb.assetNames = new string[bundle.AssetsCount()];
+        abb.addressableNames = new string[bundle.AssetsCount()];
+
+        for (var k = 0; k < bundle.AssetsCount(); k++)
+        {
+            if (bundle.GetAsset(k, out var asset))
+            {
+                abb.assetNames[k] = asset.path;
+                abb.addressableNames[k] = asset.name;
+            }
+        }
+
+        bundles.Add(abb);
+
+        var buildContent = new BundleBuildContent(bundles);
+        var dt = DateTime.Now;
+        ReturnCode exitCode = ContentPipeline.BuildAssetBundles(
+            build_params,
+            buildContent,
+            out var results
+        );
+
+        if (exitCode == ReturnCode.Success)
+        {
+            Debug.Log(
+                $"[{nameof(Content)}] Build {addon_bundle} finished! [Time: {DateTime.Now - dt}]"
+            );
+        }
+        else
+        {
+            Debug.LogError($"[{nameof(Content)}] Build error -> {exitCode}");
+        }
+    }
+
+    public void BuildMultiple(Addon addon, Bundle[] target_bundles)
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().isDirty)
+        {
+            EditorUtility.DisplayDialog(
+                "ContentDatabase - Building",
+                "Unable to start archiving! There are unsaved scenes!",
+                "Close"
+            );
+            return;
+        }
+
+        AssetDatabase.SaveAssets();
+
+        var build_params = new ContentBuildParameters(
+            EditorUserBuildSettings.activeBuildTarget,
+            BuildTargetGroup.Standalone,
+            ContentFolder
+        )
+        {
+            WriteLinkXML = WriteLinkXML,
+            TempOutputFolder = $"{ContentFolder}/.TempContent",
+            UseCache = UseCache,
+            ContentBuildFlags = BuildFlags,
+            ContiguousBundles = ContiguousBundles,
+            DisableVisibleSubAssetRepresentations = DisableVisibleSubAssetRepresentations,
+            BundleCompression = BuildCompression.Uncompressed
+        };
+        var bundles = new List<AssetBundleBuild>();
+
+        foreach (var bundle in target_bundles)
+        {
+            if(bundle.AssetsCount() < 0)
+            {
+
+                continue;
+            }
+
+            var addon_bundle = $"{addon.Name}/{bundle.name}";
+            build_params.PerBundleCompression.Add(
+                addon_bundle,
+                GetCompressionType(addon.Name, bundle.name)
+            );
+            var abb = new AssetBundleBuild();
+            abb.assetBundleName = addon_bundle;
+            abb.assetNames = new string[bundle.AssetsCount()];
+            abb.addressableNames = new string[bundle.AssetsCount()];
+
+            for (var k = 0; k < bundle.AssetsCount(); k++)
+            {
+                if (bundle.GetAsset(k, out var asset))
+                {
+                    abb.assetNames[k] = asset.path;
+                    abb.addressableNames[k] = asset.name;
+                }
+            }
+
+            bundles.Add(abb);
+        }
+
+        var buildContent = new BundleBuildContent(bundles);
+        var dt = DateTime.Now;
+        ReturnCode exitCode = ContentPipeline.BuildAssetBundles(
+            build_params,
+            buildContent,
+            out var results
+        );
+
+        if (exitCode == ReturnCode.Success)
+        {
+            Debug.Log(
+                $"[{nameof(Content)}] Build {bundles.Count} bundles finished! [Time: {DateTime.Now - dt}]"
+            );
+        }
+        else
+        {
+            Debug.LogError($"[{nameof(Content)}] Build error -> {exitCode}");
+        }
+    }
+
+    void BuildAll()
     {
         if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().isDirty)
         {
@@ -65,11 +244,18 @@ public partial class Content : ScriptableObject, IPostprocessBuildWithReport
             EditorUserBuildSettings.activeBuildTarget,
             BuildTargetGroup.Standalone,
             ContentFolder
-        );
-        build_params.BundleCompression = BuildCompression.Uncompressed;
+        )
+        {
+            WriteLinkXML = WriteLinkXML,
+            TempOutputFolder = $"{ContentFolder}/.TempContent",
+            UseCache = UseCache,
+            ContentBuildFlags = BuildFlags,
+            ContiguousBundles = ContiguousBundles,
+            DisableVisibleSubAssetRepresentations = DisableVisibleSubAssetRepresentations,
+            BundleCompression = BuildCompression.Uncompressed
+        };
 
         var total_addons_builded = 0;
-
         var total_bundles = 0;
         var total_bundles_builded = 0;
 
@@ -132,12 +318,13 @@ public partial class Content : ScriptableObject, IPostprocessBuildWithReport
 
         var bundles = new List<AssetBundleBuild>();
 
-        foreach (var k in addons)
+        foreach (var addon in addons)
         {
-            bundles.AddRange(k.Value);
+            bundles.AddRange(addon.Value);
         }
 
         var buildContent = new BundleBuildContent(bundles);
+        var dt = DateTime.Now;
         ReturnCode exitCode = ContentPipeline.BuildAssetBundles(
             build_params,
             buildContent,
@@ -167,10 +354,10 @@ public partial class Content : ScriptableObject, IPostprocessBuildWithReport
                         for (var i = 0; i < bundle_info.dependencies.Length; i++)
                         {
                             var parts = bundle_info.dependencies[i].Split('/');
-                            var temp_a_name = parts[0];
-                            var temp_b_name = parts[1];
+                            var addon_folder_name = parts[0];
+                            var bundle_path = parts[1];
 
-                            bundle_info.dependencies[i] = $"{temp_a_name}@{temp_b_name}";
+                            bundle_info.dependencies[i] = $"{addon_folder_name}@{bundle_path}";
 
                             if (parts.Length >= 3)
                             {
@@ -190,7 +377,7 @@ public partial class Content : ScriptableObject, IPostprocessBuildWithReport
             SaveToJson(true);
 
             Debug.Log(
-                $"[{nameof(Content)}] Build finished! [Addons: {total_addons_builded}/{AddonsCount()}] [Bundles: {total_bundles_builded}/{total_bundles}]"
+                $"[{nameof(Content)}] Build finished! [Addons: {total_addons_builded}/{AddonsCount()}] [Bundles: {total_bundles_builded}/{total_bundles}] [Time: {DateTime.Now - dt}]"
             );
         }
         else
